bbae2b621c2ec3bc36e59597c087a1a16b4fc076
Lena Anyusheva <elena-anyusheva@yandex.ru>
2021-06-07 20:29:37 +0300

    make writing to file one function

--- simple.cc
+++ simple.cc
@@ -7,22 +7,21 @@
 #include <string>
 namespace ss = seastar;
 
-std::string str(4096, 'x');
+constexpr size_t aligned_size = 4096;
+
+
+std::string str(aligned_size, 'x');
+ss::sstring fname = "/root/seastar-starter/simple_output.txt";
 const char * to_write = str.c_str();
 
-ss::future<> write_to_file(ss::file f) {
-	return f.dma_write<char>(0, to_write, 4096).then([](size_t unused){
-			
-    std::cout << "I wrote\n" << std::flush;
-    });
-}
 
-
-ss::future<> open_file_and_write() {
-    ss::sstring fname = "/root/seastar-starter/simple_output.txt";
-    return ss::open_file_dma(fname, ss::open_flags::wo | ss::open_flags::create).then([](ss::file f) mutable {
-		    std::cout << "opened file\n"; 
-		    return write_to_file(f);
+ss::future<> write_to_file(ss::sstring fname) {
+    return with_file(ss::open_file_dma(fname, ss::open_flags::wo | ss::open_flags::create),
+        [](ss::file f) mutable {
+            std::cout << "opened file\n";
+            return f.dma_write<char>(0, to_write, aligned_size).then([](size_t unused){
+                std::cout << "I wrote\n" << std::flush;
+            });
     });
 }
 
@@ -30,7 +29,9 @@ ss::future<> open_file_and_write() {
 int main(int argc, char** argv) {
     seastar::app_template app;
     try {
-        app.run(argc, argv, open_file_and_write);
+        app.run(argc, argv, []{
+            return write_to_file(fname);
+        });
     } catch(...) {
         std::cerr << "Failed to start application: "
                   << std::current_exception() << "\n";
