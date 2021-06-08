9464cb63062e47e062070ad8b4413cd836ae5cea
Lena Anyusheva <elena-anyusheva@yandex.ru>
2021-06-09 02:34:05 +0300

    read and write to file by chunks of 4KB in a do_for_each loop

--- simple.cc
+++ simple.cc
@@ -1,10 +1,12 @@
 #include <seastar/core/app-template.hh>
 #include <seastar/core/reactor.hh>
 #include <seastar/core/sleep.hh>
+#include <boost/iterator/counting_iterator.hpp>
 
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 namespace ss = seastar;
 
 constexpr size_t aligned_size = 4096;
@@ -13,21 +15,21 @@ constexpr size_t aligned_size = 4096;
 ss::sstring fname_output = "/root/seastar-starter/simple_output.txt";
 ss::sstring fname_input = "/root/seastar-starter/simple_input.txt";
 
-ss::future<> write_to_file(ss::temporary_buffer<char>& buf, ss::sstring fname) {
+ss::future<> write_to_file(int& pos, ss::temporary_buffer<char>& buf, ss::sstring fname) {
     return with_file(ss::open_file_dma(fname, ss::open_flags::wo | ss::open_flags::create),
-        [&buf](ss::file f) mutable {
+        [&buf, &pos](ss::file f) mutable {
             std::cout << "opened file\n";
-            return f.dma_write<char>(0, buf.get(), aligned_size).then([](size_t unused){
+            return f.dma_write<char>(pos*aligned_size, buf.get(), aligned_size).then([&pos](size_t unused){
                 std::cout << "I wrote\n" << std::flush;
             });
     });
 }
 
-ss::future<> read_from_file(ss::temporary_buffer<char>& buf, const ss::sstring& fname){
+ss::future<> read_from_file(int& pos, ss::temporary_buffer<char>& buf, const ss::sstring& fname){
     return with_file(ss::open_file_dma(fname, ss::open_flags::ro),
-        [&buf](ss::file& f) mutable {
+        [&buf, &pos](ss::file& f) mutable {
             std::cout << "opened file to read\n";
-            return f.dma_read<char>(0, buf.get_write(), aligned_size).then([&buf](size_t unused){
+            return f.dma_read<char>(pos*aligned_size, buf.get_write(), aligned_size).then([&buf, &pos](size_t unused){
                 std::cout << "I read\n";
                 for (int i = 0; i < aligned_size; ++i) {
                     std::cout << buf[i];
@@ -38,15 +40,25 @@ ss::future<> read_from_file(ss::temporary_buffer<char>& buf, const ss::sstring& 
 }
 
 int main(int argc, char** argv) {
+    using namespace std::chrono_literals;
     seastar::app_template app;
     try {
         app.run(argc, argv, []{
-            return ss::do_with(ss::temporary_buffer<char>::aligned(aligned_size, aligned_size),
-                [](auto& buf) {
-                    return read_from_file(buf, fname_input).then([&buf]{
-                        return write_to_file(buf, fname_output);
-                });
-            });
+            return ss::do_with(
+                ss::temporary_buffer<char>::aligned(aligned_size, aligned_size),
+                std::vector<int>{0, 1, 2},
+                [](auto& buf, auto & range){
+                    return ss::do_for_each(
+                        range,
+                        [&buf](int& pos) {
+                            std::cout << "pos: " << pos << "\n";
+                            return read_from_file(pos, buf, fname_input).then([&buf, &pos] {
+                                return write_to_file(pos, buf, fname_output);
+                            });
+                        }
+                    );
+                }
+            );
         });
     } catch(...) {
         std::cerr << "Failed to start application: "
